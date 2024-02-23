0c946357cbc992a8d66447cd18cc74943aca5648
C04HN <c04hn@teb.edu.pl>
2024-02-23 13:10:37 +0100

    Kod z 23.02

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from blog.models import Post
+
+
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return HttpResponse("Kajtek potrzebuje pomocy")
