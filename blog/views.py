e2e1474feb7316652e4ff9ec1538b228c58ca483
Student <rpawer2@gmail.com>
2024-03-22 13:23:02 +0100

    Kod z 01.03

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import detail
+from django.views.generic import detail, ListView
 
 import blog
 from blog.models import Post
 
 
 # Create your views here.
-def post_list(request):
-    posts = Post.published.all()  # .filter(status='published')
-    return render(request, "blog/post/list.html", {"posts": posts})
-
+# def post_list(request):
+#     posts = Post.published.all()  # .filter(status='published')
+#     return render(request, "blog/post/list.html", {"posts": posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post, slug=slug, status="published", publish__year=year, publish__month=month, publish__day=day)
