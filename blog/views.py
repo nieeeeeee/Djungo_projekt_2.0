6573e1e904262e090ab73dd50ccfbb2c859e3b80
Student <rpawer2@gmail.com>
2024-04-12 13:21:17 +0200

    Kod z 01.03

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import detail, ListView
 
-import blog
+from blog.forms import CommentForm
 from blog.models import Post
 
 
@@ -18,5 +18,14 @@ class PostListView(ListView):
 
 def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post, slug=slug, status="published", publish__year=year, publish__month=month, publish__day=day)
+    coments = post.comments.filter(active=True)
+    if request.method == "POST":
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+    coment_form = CommentForm()
+    return render(request, 'blog/post/detal.html', {"post": post, "coments": coments,"coment_form":coment_form})
 
-    return render(request, 'blog/post/detal.html', {"post": post})
+
